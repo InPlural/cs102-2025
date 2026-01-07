361f369cbcc87fdf7bd58a41de626569e387c1c1
InPlural <arinadautova21@gmail.com>
2026-01-07 18:36:05 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+alpha_size = 26
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_len = len(keyword)
+    for key_idx, char in enumerate(plaintext):
+        if "A" <= char <= "Z":
+            a_idx = ord("A")
+        elif "a" <= char <= "z":
+            a_idx = ord("a")
+        else:
+            ciphertext += char
+            continue
+
+        position = ord(char) - a_idx
+        key_char = keyword[key_idx % key_len]
+        shift = ord(key_char) - a_idx
+        new_position = position + shift
+        new_char = chr(a_idx + new_position % alpha_size)
+        ciphertext += new_char
     return ciphertext
 
 
